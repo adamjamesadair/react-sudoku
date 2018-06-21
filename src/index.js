df30b0f7233659c25e24494d3c97c595203eaafb
adamjamesadair <adam.james.adair@gmail.com>
2018-06-21 18:13:08 +0900

    Added cell class

--- src/index.js
+++ src/index.js
@@ -3,23 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 var _ = require('lodash');
 
-Array.prototype.diff = function(a) {
-  return this.filter(function(i) {
-    return a.indexOf(i) < 0;
-  });
-};
+// Array.prototype.diff = function(a) {
+//   return this.filter(function(i) {
+//     return a.indexOf(i) < 0;
+//   });
+// };
 
-function indexToCoord(i) {}
+function coordsToPositions(squares) {}
 
-function coordToPosition(coord) {}
-
-function getNeighbours(coords, squares) {
+function getNeighbours(coords, cells) {
   let neighbours = [];
   // Row and column neighbours
   for (let i = 0; i < 9; i++) {
-    neighbours.push(squares[coords[0]][i]);
-    neighbours.push(squares[i][coords[1]]);
-  }
+    let col = cells.filter(function(cell) {
+      return (cell.coords[0] == coords[0] && cell.coords[1] == i);
+    });
+    let row = cells.filter(function(cell) {
+      return (cell.coords[0] == i && cell.coords[1] == coords[1]);
+    });
+    if (col[0])
+      neighbours.push(col[0].value);
+    if (row[0])
+      neighbours.push(row[0].value);
+    }
 
   // Same block neighbours
   let iBlockStart = Math.floor(coords[0] / 3) * 3;
@@ -27,31 +33,78 @@ function getNeighbours(coords, squares) {
 
   for (let j = iBlockStart; j < iBlockStart + 3; j++) {
     for (let k = jBlockStart; k < jBlockStart + 3; k++) {
-      neighbours.push(squares[j][k]);
+      let block = cells.filter(function(cell) {
+        return (cell.coords[0] == j && cell.coords[1] == k);
+      });
+      if (block[0])
+        neighbours.push(block[0].value);
+      }
     }
-  }
   return neighbours;
 }
 
-function getSquareValue(coords, squares) {
-  let options = Array.from(Array(9).keys());
-  let [i, j] = coords;
-  let neighbours = getNeighbours(coords, squares);
-  return (_.sample(options.diff(neighbours)));
+// function getCellValue(cell, cells) {
+//   return new Promise(function(resolve) {
+//     let options = Array.from(Array(9).keys());
+//     let neighbours = getNeighbours(cell.coords, cells);
+//     resolve(_.sample(options.diff(neighbours)));
+//   });
+// }
+
+function fillCellsWrapper(cells) {
+  let emptyCells = cells;
+  fillCells(emptyCells);
+}
+
+function fillCells(emptyCells) {
+  let cell = emptyCells.shift();
+
+  let neighbours = getNeighbours(cell.coords, emptyCells);
+  let options = _.difference(Array.from(Array(9).keys()), neighbours);
+  console.log(neighbours);
+  for (let option of options) {
+    cell.value = option;
+    if (emptyCells.length == 0) {
+      return true;
+    }
+
+    if (fillCells(emptyCells)) {
+      return true;
+    }
+  }
+
+  cell.value = '';
+  emptyCells.unshift(cell);
+  return false;
+
+  // let chain = Promise.resolve();
+  // emptyCells.forEach((cell) => {
+  //   chain = chain.then(() => getCellValue(cell, emptyCells).then((cellValue) => {
+  //     cell.value = cellValue;
+  //   }));
+  //});
 }
 
 function generateStartingValues(n) {
-  let squares = new Array(9).fill().map(() => Array(9).fill(''));
+  let cells = [];
+  // init squares
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      squares[i][j] = getSquareValue([
-        i, j
-      ], squares);
+      cells.push(new Cell([i, j]));
     }
   }
-  console.log(squares);
+
+  fillCellsWrapper(cells);
+  console.log(cells);
   // TODO Return convert to positions
   return Array(81).fill('');
+}
+
+class Cell {
+  constructor(coords) {
+    this.coords = coords;
+    this.value = '';
+  }
 }
 
 class Square extends React.Component {
